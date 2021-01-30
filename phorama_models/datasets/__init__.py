dba3f9e929f9ed2f6bb81b827f13f55e2670979f
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2021-01-30 13:56:38 +0100

    Update __init__.py

--- phorama_models/datasets/__init__.py
+++ phorama_models/datasets/__init__.py
@@ -2,4 +2,4 @@ from . import coco2017
 from . import visualgenome
 from . import indoorscene
 from . import labelme_12_50k
-from . downloads import getAndExtract
+from .downloads import getAndExtract
